476352396eaafba5b5c0d2e79ab253650f85ba5d
diy0r <sabirovdiyorkind@gmail.com>
2024-08-08 19:09:45 +0500

    perf(test): create Vertex and findOne

--- test/graph.test.ts
+++ test/graph.test.ts
@@ -3,9 +3,10 @@ import assert from 'node:assert';
 
 import test from 'node:test';
 const graph = FileGraph('./data.txt');
-test('create node and findById', async t => {
+test('create Vertex and findOne', async t => {
   const model = { name: 'Dyor', age: new Date().toString() };
   const id = await graph.createVertex(model);
-  const finModel = await graph.findById(id);
-  assert.deepStrictEqual(finModel[id], model);
+  const finModel = await graph.findOne<typeof model>(model => model.id == id);
+  console.log(finModel);
+  assert.deepStrictEqual({ id, ...model }, finModel);
 });
