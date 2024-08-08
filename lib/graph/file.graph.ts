2fac83dd7aff0b09bf022896a7663c03eb345bd3
diy0r <sabirovdiyorkind@gmail.com>
2024-08-09 00:29:05 +0500

    feat(lib): add public method updateVertex

--- lib/graph/file.graph.ts
+++ lib/graph/file.graph.ts
@@ -1,19 +1,23 @@
-import { UUID } from 'crypto';
-import { FileGraphAbstract, IVertex } from '../interfaces';
+import { FileGraphAbstract, IVertex, uuidType } from '../interfaces';
 import { StorageFile } from './storage.file';
 import { uuid } from '../utils';
 
-class FileGraphIml<T extends IVertex> implements FileGraphAbstract {
+class FileGraphIml implements FileGraphAbstract {
   constructor(private readonly storageFile: StorageFile) {}
-  public async createVertex<T extends object>(model: T): Promise<UUID> {
+  public async createVertex<T extends object>(vertex: T): Promise<uuidType> {
     const id = uuid();
-    await this.storageFile.appendFile({ id, ...model });
+    await this.storageFile.appendFile({ id, ...vertex });
     return id;
   }
   public async findOne<T extends object>(
-    predicate: (model: T & IVertex) => boolean,
+    predicate: (vertex: T & IVertex) => boolean,
   ): Promise<(T & IVertex) | null> {
     return this.storageFile.searchLine(predicate);
+  }
+  public updateVertex<T extends object>(
+    updater: (vertex: T & IVertex) => (T & IVertex) | object,
+  ): Promise<boolean> {
+    return this.storageFile.updateLine(updater);
   }
 }
 
--- lib/interfaces/params.interface.ts
+++ lib/interfaces/params.interface.ts
@@ -3,3 +3,4 @@ import { UUID } from 'crypto';
 export interface IVertex {
   id: UUID;
 }
+export type uuidType = UUID;
