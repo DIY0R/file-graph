93164d9fd1de2929a7054e37d33337c39c182093
diy0r <sabirovdiyorkind@gmail.com>
2024-08-10 00:07:51 +0500

    style(test):  better readability and accuracy

--- test/graph.test.ts
+++ test/graph.test.ts
@@ -2,29 +2,34 @@ import assert from 'node:assert';
 import { describe, it } from 'node:test';
 import { FileGraph } from 'lib';
 
-const graph = FileGraph('./data.txt');
+const graph = FileGraph('data.txt');
 const myVertex = { name: 'Diy0r', age: new Date().toString() };
-let globId = '6c5febc1-161b-46c7-abf6-b20088fd410e';
-describe('CRUD', t => {
-  it('create Vertex and findOne', async t => {
+let globId = '';
+
+describe('FileGraph CRUD Operations', () => {
+  it('should create a vertex and find it by ID', async () => {
     const createdVertexId = await graph.createVertex(myVertex);
-    const findVertex = await graph.findOne<typeof myVertex>(
-      vertex => vertex.id == createdVertexId,
+    const foundVertex = await graph.findOne<typeof myVertex>(
+      vertex => vertex.id === createdVertexId,
     );
-    assert.deepStrictEqual({ id: createdVertexId, ...myVertex }, findVertex);
-    globId = findVertex.id;
-  });
-  it('update Vertex', async t => {
-    const isUpdate = await graph.updateVertex<typeof myVertex>(model =>
-      model.id == globId ? { name: 'Dupo', id: '121212', sdsds: 'sdsd' } : null,
-    );
-    assert.equal(isUpdate, true);
+
+    assert.deepStrictEqual({ id: createdVertexId, ...myVertex }, foundVertex);
+    globId = createdVertexId;
   });
 
-  it('check updated vertex name', async t => {
-    const findVertex = await graph.findOne<typeof myVertex>(
-      vertex => vertex.id == globId,
+  it('should update the vertex name', async () => {
+    const isUpdated = await graph.updateVertex<typeof myVertex>(vertex =>
+      vertex.id === globId ? { ...vertex, name: 'Dupo' } : null,
     );
-    assert.deepStrictEqual(findVertex.name, 'Dupo');
+
+    assert.strictEqual(isUpdated, true);
+  });
+
+  it('should verify the updated vertex name', async () => {
+    const foundVertex = await graph.findOne<typeof myVertex>(
+      vertex => vertex.id === globId,
+    );
+
+    assert.strictEqual(foundVertex?.name, 'Dupo');
   });
 });
