18ee5802143e56254a4d7b495e9418c1d41facec
diy0r <sabirovdiyorkind@gmail.com>
2024-08-11 02:02:57 +0500

    refactor(lib): define callback function type

--- lib/interfaces/params.interface.ts
+++ lib/interfaces/params.interface.ts
@@ -4,3 +4,8 @@ export interface IVertex {
   id: UUID;
 }
 export type uuidType = UUID;
+
+export type IPredicate<T extends object> = (vertex: T & IVertex) => boolean;
+export type IUpdater<T extends object> = (
+  vertex: T & IVertex,
+) => (T & IVertex) | object;
