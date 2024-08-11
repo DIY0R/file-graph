b1276cd2ab0eef03e8bc2bc09c097c83e01f8aaa
diy0r <sabirovdiyorkind@gmail.com>
2024-08-11 15:38:52 +0500

    chore: update update rules for max length and line breaks

--- commitlint.config.js
+++ commitlint.config.js
@@ -16,8 +16,12 @@ export default {
         'revert',
       ],
     ],
+
     'scope-enum': [2, 'always', ['lib', 'test']],
     'scope-empty': [0, 'never'],
+    'header-max-length': [2, 'always', 70],
+    'body-max-line-length': [0, 'never'],
+    'body-leading-blank': [0, 'never'],
     'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
   },
 };
