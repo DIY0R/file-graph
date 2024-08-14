89ac152744b2e2eb7eb03177b3623740991d3496
diy0r <sabirovdiyorkind@gmail.com>
2024-08-14 19:32:41 +0500

    refactor: update jsdoc

--- lib/interfaces/graph.interface.ts
+++ lib/interfaces/graph.interface.ts
@@ -1,23 +1,23 @@
-import { IPredicate, IUpdater, uuidType } from './params.interface';
+import { IPredicate, IUpdater, IVertex, uuidType } from './params.interface';
 
 export abstract class FileGraphAbstract {
   /**
    * Creates a node in the graph.
-   * @template T - The type of the data vertex representing the node.
-   * @param {T} vertex - The data vertex for creating the node.
+   * @template T The date type from the node, which must match the type of the object.
+   * @param {T} data - The data vertex for creating the node.
    * @returns {crypto.UUID} - The unique identifier of the created node.
    */
-  public abstract createVertex<T extends object>(vertex: T): Promise<uuidType>;
+  public abstract createVertex<T extends object>(data: T): Promise<uuidType>;
 
   /**
    * Searches for a vertex in the storage file that matches the given predicate.
-   *
+   * @template T The date type from the node, which must match the type of the object.
    * @param {function(T): boolean} predicate - A function that takes a vertex as input and returns `true` if the vertex matches the search criteria, otherwise `false`.
    * @returns {Promise<T | null>} A promise that resolves to the found vertex if a match is found, or `null` if no match is found.
    */
   public abstract findOne<T extends object>(
     predicate: IPredicate<T>,
-  ): Promise<object | null>;
+  ): Promise<IVertex<T> | null>;
   /**
    * Updates a vertex record in the storage that matches the given condition.
    *
@@ -25,8 +25,8 @@ export abstract class FileGraphAbstract {
    * and updates it by replacing the old record with the new one. The `updater` function should return the updated
    * vertex object if it matches the condition.
    *
-   * @template T The type of the vertex object, which must conform to the {@link IVertex} interface.
-   * @param {Function} updater A function that takes a vertex object {@link T & IVertex} and returns an updated vertex object {@link T & IVertex}.
+   * @template T The date type from the node, which must match the type of the object.
+   * @param {Function} updater A function that takes a vertex object {@link  IVertex<T>} and returns an updated vertex object {@link  IVertex<T>}.
    * @returns {Promise<boolean>} A promise that resolves to `true` if the record was successfully updated, and `false` if the record was not found or an error occurred.
    */
   public abstract updateVertex<T extends object>(
@@ -40,7 +40,7 @@ export abstract class FileGraphAbstract {
    * and deletes it if the condition is met. The `predicate` function should return `true` for the vertex
    * that needs to be deleted.
    *
-   * @template T The type of the vertex object, which must conform to the {@link IVertex} interface.
+   * @template T The date type from the node, which must match the type of the object.
    * @param {Function} predicate A function that takes a vertex object {@link T & IVertex} and returns `true` if the vertex should be deleted.
    * @returns {Promise<boolean>} A promise that resolves to `true` if the record was successfully deleted, and `false` if the record was not found or an error occurred.
    */
