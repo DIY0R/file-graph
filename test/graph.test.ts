2d137795dd0d8f2e47169439bac86b65270c6287
diy0r <sabirovdiyorkind@gmail.com>
2024-08-16 03:18:36 +0500

    test: create an arc between two vertice

--- test/graph.test.ts
+++ test/graph.test.ts
@@ -1,12 +1,12 @@
 import assert from 'node:assert';
 import { describe, it } from 'node:test';
-import { FileGraph } from 'lib';
+import { FileGraph, uuidType } from 'lib';
 
 const graph = FileGraph('data.txt');
 const data = { name: 'Diy0r', age: new Date().toString() };
 let globId = '';
 
-describe('FileGraph CRUD Operations', () => {
+describe('Vertex CRUD Operations', () => {
   it('should create a vertex and find it by ID', async () => {
     const createdVertexId = await graph.createVertex(data);
     const foundVertex = await graph.findOne<typeof data>(
@@ -24,7 +24,6 @@ describe('FileGraph CRUD Operations', () => {
     const isUpdated = await graph.updateVertex<typeof data>(vertex =>
       vertex.id === globId ? { data: { name: 'Dupo' } } : null,
     );
-
     assert.strictEqual(isUpdated, true);
   });
 
@@ -43,5 +42,26 @@ describe('FileGraph CRUD Operations', () => {
     );
 
     assert.equal(deleteVertex, true);
+  });
+});
+
+describe('Arc operations', () => {
+  it('should create an arc between two vertices', async () => {
+    const createdVertexId = await graph.createVertex(data);
+
+    const newArcCreated = await graph.createArc(
+      globId as uuidType,
+      createdVertexId,
+    );
+    const foundVertex = await graph.findOne<typeof data>(
+      vertex => vertex.id === globId,
+    );
+
+    assert.equal(newArcCreated, true, 'Arc creation failed');
+    assert.equal(
+      foundVertex.arcs.includes(createdVertexId),
+      true,
+      'Arc not found in vertex',
+    );
   });
 });
